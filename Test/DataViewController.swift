4c0f36c55ff4f93068490755aef7583d6bf1dfcf
MishaFu <58786366+MishaFu@users.noreply.github.com>
2021-02-20 18:05:12 -0800

    Initial Commit

---
+++ Test/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Test
+//
+//  Created by helen fu on 2/20/21.
+//  Copyright © 2021 helen fu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
